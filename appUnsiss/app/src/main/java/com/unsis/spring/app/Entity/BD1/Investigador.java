8d2e52dfff3ae6cdde03e6528d6bc7e3f61f180e
Ivan Bustamante <lupitobtte_17@hotmail.com>
2024-06-21 09:12:25 -0600

    Correcciones
    Correcciones

--- appUnsiss/app/src/main/java/com/unsis/spring/app/Entity/BD1/Investigador.java
+++ appUnsiss/app/src/main/java/com/unsis/spring/app/Entity/BD1/Investigador.java
@@ -2,13 +2,11 @@ package com.unsis.spring.app.Entity.BD1;
 
 import com.unsis.spring.app.User.User;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
