67a722ff5569a5ec3125ab6746c7b4fc14d8ab4e
Ivan Bustamante <lupitobtte_17@hotmail.com>
2024-06-21 11:28:46 -0600

    Entity de Tipo de publicacion
    Creacion del Entity de Tipo de publicacion

---
+++ appUnsiss/app/src/main/java/com/unsis/spring/app/Entity/BD1/Tipo_Publicacion.java
@@ -0,0 +1,27 @@
+package com.unsis.spring.app.Entity.BD1;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="tipo_publicacion")
+public class Tipo_Publicacion {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_publicacion_tipo;
+
+    @Column(name = "descripcion_publicacion_tipo", nullable = false, columnDefinition = "TEXT")
+    private String descripcion_publicacion_tipo;
+
+}
