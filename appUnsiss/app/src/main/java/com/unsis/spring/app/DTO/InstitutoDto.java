5e3fd8163d301fdc46af95413687d7d8b878eca2
Ivan Bustamante <lupitobtte_17@hotmail.com>
2024-07-08 11:04:52 -0600

    DTO Instituto
    Se creo el DTO de instituto

---
+++ appUnsiss/app/src/main/java/com/unsis/spring/app/DTO/InstitutoDto.java
@@ -0,0 +1,15 @@
+package com.unsis.spring.app.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InstitutoDto {
+
+    private Long id;
+    private String nombre;
+
+}
