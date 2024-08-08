a8742ca7ef318a50f414453730eb3fa024bf7d9e
Ivan Bustamante <lupitobtte_17@hotmail.com>
2024-08-08 10:27:10 -0600

    Trimestre Automatico
    Se genera en automático el trimestre el primer día del mes

--- appUnsiss/app/src/main/java/com/unsis/spring/app/AppApplication.java
+++ appUnsiss/app/src/main/java/com/unsis/spring/app/AppApplication.java
@@ -2,8 +2,10 @@ package com.unsis.spring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AppApplication {
 
 	public static void main(String[] args) {
