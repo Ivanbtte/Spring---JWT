5476b7e72e819219e2140f8ea4a8a3c5b71fd5f6
Guillermo Daniel Cruz Ortega <guilledani@live.com.mx>
2024-08-16 11:48:18 -0600

    Habilitar Usuarios
    Habilita los investigadores a partir de los usuarios

--- appUnsiss/app/src/main/java/com/unsis/spring/app/Repository/BD1/InvestigadorDao.java
+++ appUnsiss/app/src/main/java/com/unsis/spring/app/Repository/BD1/InvestigadorDao.java
@@ -2,7 +2,6 @@ package com.unsis.spring.app.Repository.BD1;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,4 +21,11 @@ public interface InvestigadorDao extends JpaRepository<Investigador, Long> {
 
     @Query(value = "SELECT u.username FROM Investigador i JOIN usuario u ON i.id_usuario = u.id WHERE i.id_instituto = :institutoId AND u.role = 'COORDINADOR' AND u.enabled = true", nativeQuery = true)
     Optional<String> findCoordinadorEmailByInstitutoId(@Param("institutoId") Long institutoId);
+
+    @Query(value = "SELECT i.* FROM investigador i JOIN usuario u ON i.id_usuario = u.id WHERE u.enabled = true", nativeQuery = true)
+    List<Investigador> findInvestigadoresHabilitados();
+
+    @Query(value = "SELECT i.* FROM investigador i JOIN usuario u ON i.id_usuario = u.id WHERE u.enabled = true AND i.id_instituto = :id", nativeQuery = true)
+    List<Investigador> findInvestigadoresHabilitadosPorInstituto(@Param("id") Long idInstituto);
+
 }
