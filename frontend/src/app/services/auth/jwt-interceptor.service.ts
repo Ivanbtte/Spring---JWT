53d8175688b74c43874ab46078bed82f7b1f3fff
Ivan Bustamante <lupitobtte_17@hotmail.com>
2024-09-03 10:29:29 -0600

    Cookies y npm install
    Se instala la dependencia para las cookies

--- frontend/src/app/services/auth/jwt-interceptor.service.ts
+++ frontend/src/app/services/auth/jwt-interceptor.service.ts
@@ -2,6 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,10 @@ export class JwtInterceptorService implements HttpInterceptor{
   private excludedUrls = ['/api/v1/files/upload']; // Agrega aquí cualquier URL que no deba tener JWT
 
 
-  constructor(private loginService:LoginService) { }
+  constructor(private loginService: LoginService, private cookieService: CookieService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token:String=this.loginService.userToken;
+    let token = this.cookieService.get('token') || this.loginService.userToken;
 
     if (token && !this.excludedUrls.some(url => req.url.includes(url))) {
       req = req.clone({
